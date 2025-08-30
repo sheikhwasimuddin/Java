af11c886ed27d83cbe613d5f5697fe562bbda0ff
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-08-30 23:55:59 +0530

    2D array Creation

---
+++ 2D array/Creation.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+public class Creation {
+    public static void main(String[] args) {
+        //initialisation
+        int arr[][]= new int[3][3];
+        int n=arr.length;
+        int m=arr[0].length;
+
+        //input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements of the array:");
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //output
+        System.out.println("the array is:");
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+    
