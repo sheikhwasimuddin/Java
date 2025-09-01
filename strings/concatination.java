9cd1b47d005c88696595ed7f911703b4e41907cd
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-01 17:20:48 +0530

    concatenation

---
+++ strings/concatination.java
@@ -0,0 +1,8 @@
+public class concatination {
+    public static void main(String[] args) {
+        String str1 = "cristiano ronaldo";
+        String str2 = "siuuuuuuu.......";
+        String result = str1 + " " + str2;
+        System.out.println(result);
+    }
+}
