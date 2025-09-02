92fa856aab9eced1032198709c25e5fb76ed0682
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 19:39:07 +0530

    Bitwise OR

---
+++ bit manipulation/or.java
@@ -0,0 +1,8 @@
+public class or {
+    public static void main(String[] args) {
+        int a = 5;  // 0101 in binary
+        int b = 3;  // 0011 in binary
+        int result = a | b;  // Bitwise OR operation
+        System.out.println("The result of " + a + " OR " + b + " is: " + result);
+    }
+}
