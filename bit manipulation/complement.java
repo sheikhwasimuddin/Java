24fa06b5f6ebb789c9041cd50c389101805badaf
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 21:24:50 +0530

    Complement Bit Manipulation

---
+++ bit manipulation/complement.java
@@ -0,0 +1,10 @@
+public class complement {
+    public static void main(String[] args) {
+        int n = 5; // Example number
+        int complement = ~n;
+        
+        System.out.println("The complement of " + n + " is: " + complement);
+        
+        System.out.println("The complement of 0 is: " + ~0);
+    }
+}
