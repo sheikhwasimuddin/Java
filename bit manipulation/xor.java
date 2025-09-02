d81af6262621c91bc8eb3dd346820d61ed863d61
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 19:41:53 +0530

    Bitwise XOR

---
+++ bit manipulation/xor.java
@@ -0,0 +1,8 @@
+public class xor {
+    public static void main(String[] args) {
+        int a = 5;  // 0101 in binary
+        int b = 3;  // 0011 in binary
+        int result = a ^ b;  // Bitwise XOR operation
+        System.out.println("The result of " + a + " XOR " + b + " is: " + result);
+    }
+}
