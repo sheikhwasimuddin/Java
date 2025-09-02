1730cfebe401f7434c2ffb6845c2e961f9b25c09
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 21:52:51 +0530

    Right  Shift Bit manipulation

---
+++ bit manipulation/rightshift.java
@@ -0,0 +1,8 @@
+public class rightshift {
+    public static void main(String[] args) {
+        int n = 6;// Example number
+        int result = n >> 1;// Right shift by 1
+        System.out.println("The result of right shifting " + n + " by 1 is: " + result);
+        //a>>b=a/(2^b)
+    }
+}
