09adb4eb6d6b93367506c3a09bc748ac77b06455
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 19:06:27 +0530

    String Compression problem 2nd approach

--- strings/StringCompression.java
+++ strings/StringCompression.java
@@ -3,7 +3,14 @@ public class StringCompression {
         String str="aaabbccdee";
         String output=compress(str);
         System.out.println(output);
+        String output1=compressStringBuilder(str);
+        System.out.println(output1);
     }
+
+    /**
+     * O(n)
+     * 1st approach
+     */
     public static String compress(String str){
         String newStr="";
         
@@ -21,5 +28,22 @@ public class StringCompression {
 
         }
         return newStr;
+    }
+
+    //2nd approach
+    public static String compressStringBuilder(String str){
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            int count=1;
+            while(i<str.length()-1&&str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count);
+            }
+        }
+        return sb.toString();
     }
 }
