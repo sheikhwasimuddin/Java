92dc14e3d44325a6718f807e0479daf901a803ad
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 19:35:02 +0530

    Bitwise AND

---
+++ bit manipulation/and.java
@@ -0,0 +1,8 @@
+public class and {
+    public static void main(String[] args) {
+        int a = 5;  // 0101 in binary
+        int b = 3;  // 0011 in binary
+        int result = a & b;  // Perform bitwise AND
+        System.out.println("Bitwise AND of " + a + " and " + b + " is: " + result);
+    }
+}
