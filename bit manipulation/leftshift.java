5964ef8a8c3078829dd1e48d9cee1be7acf61252
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-09-02 21:36:24 +0530

    Left Shift Bit manipulation

---
+++ bit manipulation/leftshift.java
@@ -0,0 +1,7 @@
+public class leftshift {
+    public static void main(String[] args) {
+        int n = 5; // Example number
+        int result = n << 2; // Left shift by 2
+        System.out.println("The result of left shifting " + n + " by 2 is: " + result);
+    }
+}
