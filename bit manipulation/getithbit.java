8a625a2ed4fb2c7cbef4a529789a85545e6c770b
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2025-10-11 21:24:01 +0530

    get ith bit

--- bit manipulation/getithbit.java
+++ bit manipulation/getithbit.java
@@ -1,9 +1,11 @@
 public class getithbit {
     public static void main(String[] args) {
-        
+        int n=10;
+        int i=1;
+        System.out.println(getith(n,i));
     }
     public static int getith(int n,int i){
-        int bitmask=i<<i;
+        int bitmask=i<<1;
         if((n&bitmask)==0){
             return 0;
         } else {
